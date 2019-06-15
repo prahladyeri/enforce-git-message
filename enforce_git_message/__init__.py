ee565c0ee33991138df2114b6dd95e3a65a0ac0f
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-15 16:23:40 +0530

    fix: issues in commit-msg script

--- enforce_git_message/__init__.py
+++ enforce_git_message/__init__.py
@@ -4,5 +4,5 @@ __copyright__ = "(c) 2019 Prahlad Yeri"
 
 __license__ = "MIT"
 __version__ = "1.0.0"
-__title__ = 'Enforce GIT Messages'
+__title__ = 'Enforce GIT Message'
 __description__ = 'Enforces conventional git commit messages for git repositories'
--- enforce_git_message/main.py
+++ enforce_git_message/main.py
@@ -1,10 +1,9 @@
 import shutil
-
 def main():
 	if shutil.which('git') == None:
-		print('git not found in path. please install git and then reinstall enforce-git-messages')
-	
-	print("Hello, World")
+		print('git not found on path. please install git and then run pip install --upgrade enforce-git-message')
+		return
+	os.path.exists("~/.git-templates/hooks/commit-msg")
 
 if __name__ == "__main__":
 	main()
