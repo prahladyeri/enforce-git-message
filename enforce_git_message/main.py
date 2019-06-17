58392bd00b719a08efb3b0642ed8022133891b23
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-17 08:54:38 +0530

    fix(main): added graceful handling of exception when .git folder isn't found on current path

--- enforce_git_message/main.py
+++ enforce_git_message/main.py
@@ -1,6 +1,10 @@
 import shutil, os
 
 def main():
+	if not os.path.isdir('.git'):
+		print('error: .git directory not found in the current path. this is not a git repository?')
+		return
+	
 	template_path = os.path.expanduser("~/.git-templates/hooks/commit-msg")
 	if shutil.which('git') == None:
 		print('error: git not found on path. please install git and then run pip install --upgrade enforce-git-message')
