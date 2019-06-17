8c324882b5e345a3f21add59b7ea1f3037ce3034
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-17 08:59:06 +0530

    chore: released v1.0.1

--- enforce_git_message/__init__.py
+++ enforce_git_message/__init__.py
@@ -3,6 +3,6 @@ __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
 __license__ = "MIT"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __title__ = 'Enforce GIT Message'
 __description__ = 'Enforces conventional git commit messages for git repositories'
