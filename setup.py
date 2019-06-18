439a1d50ce538295ea3a00e5164139bc586b2fb8
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-19 03:53:15 +0530

    fix: fixed variable naming issue in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import site
 from distutils.dir_util import copy_tree
 #import shutil
 
-app_name = "enforce-git-message"
+pypi_name = "enforce-git-message"
 pkg_name = "enforce_git_message"
 
 class PostInstallCommand(install):
@@ -19,7 +19,7 @@ class PostInstallCommand(install):
 		os.system("echo CUSTOM_PRE_MESSAGE")
 		install.run(self)
 		os.system("echo CUSTOM_POST_MESSAGE")
-		#conf_path_temp = resource_filename(Requirement.parse(app_name), "git-templates")
+		#conf_path_temp = resource_filename(Requirement.parse(pypi_name), "git-templates")
 		#the_path = site.getusersitepackages() # => Useless and misleading
 		fpath = os.path.join(self.install_lib, pkg_name)
 		fpath = os.path.join(fpath, "git-templates")
@@ -31,14 +31,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 s = setup(
-	name=app_name,
+	name=pypi_name,
 	version=__version__,
 	license=__license__,
 	description=__description__,
 	long_description=read("README.md"),
 	long_description_content_type='text/markdown',
 	keywords="security,scanner",
-	url='https://github.com/prahladyeri/%s' % app_name,
+	url='https://github.com/prahladyeri/%s' % pypi_name,
 	packages=find_packages(),
 	include_package_data=True,
 	entry_points={
