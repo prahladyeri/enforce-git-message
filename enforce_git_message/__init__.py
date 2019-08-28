c78504dba5fd56bed2836c7bd8476a0183871234
toxicmender <13049649+toxicmender@users.noreply.github.com>
2019-08-28 19:01:27 +0530

    docs(__description__): Adds link to conventions commits website for further reading

--- enforce_git_message/__init__.py
+++ enforce_git_message/__init__.py
@@ -5,4 +5,4 @@ __copyright__ = "(c) 2019 Prahlad Yeri"
 __license__ = "MIT"
 __version__ = "1.0.1"
 __title__ = 'Enforce GIT Message'
-__description__ = 'Enforces conventional git commit messages for git repositories'
+__description__ = 'Enforces conventional git commit messages for git repositories. Read more at https://www.conventionalcommits.org/en/v1.0.0-beta.4/'
